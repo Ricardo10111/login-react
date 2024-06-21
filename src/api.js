80ab23738bf829cc9779c1f71c77bf3046d7f0a1
Ricardo Lopez <albelop1m@gmail.com>
2024-06-21 16:31:13 -0600

    chore: add changes in all the project

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const API_URL = 'https://backend-challenge-api-3k5h.onrender.com'
+
+export function getSubmit(data) {
+  return fetch(`${API_URL}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data) // Directamente los datos
+  })
+}
